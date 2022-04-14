670fae1ebbcaa3c2065be19137570d6940db4c96
Ezra <gerkibz@outlook.com>
2022-04-14 11:26:06 +0300

    update package imports

--- lib/pages/apply/first_time/for_sme/for_sme4.dart
+++ lib/pages/apply/first_time/for_sme/for_sme4.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/configs/colors.dart';
 import 'package:flutter_application_1/configs/locator.dart';
